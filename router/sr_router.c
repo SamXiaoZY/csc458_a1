79ac088d852eafdd8e79130ee263cb0e375291c1
Fan Chongjian <fanchon1@cdf.toronto.edu>
2016-10-20 21:08:31 -0400

    small changes to handle packet reply

--- router/sr_router.c
+++ router/sr_router.c
@@ -189,36 +189,34 @@ void sr_handle_packet_reply(struct sr_instance* sr, uint8_t *ip_packet, struct s
   } else if (ip_hdr->ip_p == ip_protocol_icmp && cksum(ip_hdr, ip_hdr->ip_len)) {
     /* If the packet is a valid ICMP echo request, send an echo reply through a icmp_header*/
     struct sr_rt* longestPrefixIPMatch = getInterfaceLongestMatch(sr->routing_table,ip_hdr->ip_src);
-    struct sr_arpentry *arp_entry = sr_arpcache_lookup(&(sr->cache), ip_hdr->ip_dst);
-
+    uint32_t nextHopIPHardware = ntohl(longestPrefixIPMatch->gw.s_addr); 
+    struct sr_arpentry *arp_entry = sr_arpcache_lookup(&(sr->cache), ntohl(longestPrefixIPMatch->gw.s_addr));
+   
     unsigned int headers_size = sizeof(sr_ip_hdr_t) + sizeof(sr_icmp_hdr_t);
     uint8_t* icmp_payload = ip_packet + headers_size;
     sr_object_t icmp_wrapper = create_icmp_packet(icmp_type_echo_reply, icmp_code_0, icmp_payload, ip_hdr->ip_len - headers_size);
-    sr_object_t ip_wrapper = create_ip_packet(ip_protocol_icmp, ip_src, ip_dest, icmp_wrapper.packet, icmp_wrapper.len);
+    sr_object_t ip_wrapper = create_ip_packet(ip_protocol_icmp, ip_src, nextHopIPHardware, icmp_wrapper.packet, icmp_wrapper.len);
     sr_object_t eth_wrapper = create_ethernet_packet(eth_src,eth_dest,ethertype_ip,ip_wrapper.packet,ip_wrapper.len);
     if (longestPrefixIPMatch == NULL) {
       sr_object_t icmp_t3_wrapper = create_icmp_t3_packet(icmp_type_dest_unreachable, icmp_code_0, 0, ip_packet);
       createAndSendIPPacket(sr, ip_src, ip_dest, eth_src, eth_dest, icmp_t3_wrapper.packet, icmp_t3_wrapper.len);
     } else if (arp_entry == NULL) {
-      sr_arpcache_queuereq(&(sr->cache), ip_dest, eth_wrapper.packet, eth_wrapper.len, longestPrefixIPMatch->interface);
+      sr_arpcache_queuereq(&(sr->cache), nextHopIPHardware, eth_wrapper.packet, eth_wrapper.len, longestPrefixIPMatch->interface);
     }
     else{
+      struct sr_if* outgoing_interface = sr_get_interface(sr, longestPrefixIPMatch->interface);
+      eth_src = outgoing_interface->addr;
+      eth_dest = arp_entry->mac;
 
-    struct sr_if* outgoing_interface = sr_get_interface(sr, longestPrefixIPMatch->interface);
-    eth_src = outgoing_interface->addr;
-    eth_dest = arp_entry->mac;
-
-    uint8_t* hardware_ether_src = malloc(6);
-    memcpy(hardware_ether_src, eth_src, 6);
-    swap_mac(hardware_ether_src);
-
-    createAndSendIPPacket(sr, ip_src, ip_dest, hardware_ether_src, eth_dest, icmp_wrapper.packet, icmp_wrapper.len);
+      uint8_t* hardware_ether_src = malloc(6);
+      memcpy(hardware_ether_src, eth_src, 6);
+      swap_mac(hardware_ether_src);
+      createAndSendIPPacket(sr, ip_src, nextHopIPHardware, hardware_ether_src, eth_dest, icmp_wrapper.packet, icmp_wrapper.len);
     }
     free(icmp_wrapper.packet);
     free(ip_wrapper.packet);
     free(eth_wrapper.packet); 
  }
-
 }
 
 
